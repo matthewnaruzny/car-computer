f826f24dc3200c9f7a216bf8f7bcff9006ae1b32
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-01-03 19:25:28 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from modemUnit import RemoteCommunication
+
+if __name__ == '__main__':
+    # Starts Comms
+    remote = RemoteCommunication()
+    # Start Crash Monitoring
+    pass
---
+++ modemUnit.py
@@ -0,0 +1,8 @@
+class RemoteCommunication:
+
+    def __init__(self):
+        pass
+
+    def send(self, payload):
+        pass
+

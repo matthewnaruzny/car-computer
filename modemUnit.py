06f572fef33957452283d71536e0f45bb0d085ef
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-01-28 23:38:50 -0700

    Modified checks

--- modemUnit.py
+++ modemUnit.py
@@ -10,15 +10,10 @@ class RemoteCommunication:
 
     def start_network(self):
         print("Attempting to connect to network...")
-        self.pon_p = subprocess.Popen(['sudo', 'pon'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.pon_p = subprocess.Popen(['sudo', 'pon'])
 
         while True:
             time.sleep(5)
-            stdout, stderr = self.pon_p.communicate()
-            output = stdout.decode('utf-8')
-            if "failed" in output:
-                print("Failed... retrying")
-                self.pon_p = subprocess.Popen(['sudo', 'pon'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
             # Check if ppp0 exists
             print("Checking...")

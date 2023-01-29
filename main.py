d935f8d94a0b8b6141b1b68b930770ae8afec22d
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-01-28 17:55:37 -0700

    Added modem start and stop methods

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from modemUnit import RemoteCommunication
 if __name__ == '__main__':
     # Starts Comms
     remote = RemoteCommunication()
-    # Start Crash Monitoring
-    pass
+    remote.start_network()
--- modemUnit.py
+++ modemUnit.py
@@ -1,8 +1,36 @@
+import subprocess
+import threading
+import time
+
+
 class RemoteCommunication:
 
     def __init__(self):
-        pass
+        self.pon_p = None
+
+    def start_network(self):
+        self.pon_p = subprocess.Popen(['sudo', 'pon'])
+        time.sleep(5)
+
+        # Check if ppp0 is created
+        r = subprocess.Popen(['ip', 'addr', 'show'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = r.communicate()
+        assert isinstance(stdout[0], bytes)
+        if "ppp0" in stdout.decode("utf-8"):
+            # Create network route
+            subprocess.call(['sudo', 'route', 'add', '-net', '"0.0.0.0"', 'ppp0'])
+            self.pon_p = None
+
+    def stop_network(self):
+        if self.pon_p is not None:
+            subprocess.call(['sudo', 'poff'])
+            self.pon_p = None
 
     def send(self, payload):
         pass
 
+    def start_call(self):
+        pass
+
+    def receive_call(self):
+        pass

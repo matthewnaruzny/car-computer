2cb2cf067a355f0df4adafb1b0a93176d750ff8d
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-02-17 15:07:34 -0700

    Updated Example Config

--- config_example.py
+++ config_example.py
@@ -1,6 +1,21 @@
 sip_config = {
     'sip_host': '',
-    'sip_port': 5061,
+    'sip_port': 5060,
     'sip_username': '',
     'sip_password': ''
+}
+
+tunnel_config = {
+    'start_cmd': '',
+    'check_ip': ""
+}
+
+mqtt_config = {
+    'url': '',
+    'port': 1883,
+    'root_cert': '',
+    'client': '',
+    'username': '',
+    'password': '',
+    'topic': ''
 }

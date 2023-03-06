4eaef005ac16f9173b5a089bdc977e7c30c2622a
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-03-05 20:24:52 -0700

    Added method to get networks

---
+++ locate.py
@@ -0,0 +1,20 @@
+import subprocess
+
+
+def get_networks():
+    networks = []
+    result = subprocess.check_output(["sudo", "iwlist", "wlan0", "scan"])
+    result = result.decode('ascii')
+    result = result.split('\n')
+    for i in range(len(result)):
+        line = result[i]
+        if "Cell" in line:
+            bssid_s = line.split(":")
+            bssid = bssid_s[0][1:]
+            for s in bssid_s[1:]:
+                bssid = bssid + ":" + s
+            channel = result[i+1].split(":")[1]
+            networks.append({"macAddress": bssid, "channel": int(channel)})
+    return networks
+
+

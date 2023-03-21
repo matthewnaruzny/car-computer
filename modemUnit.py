e0d7f0c1779be91c40a5850337cf7c5a5ad6fd21
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-03-21 17:41:08 -0600

    Added HTTP GET through GPRS

--- modemUnit.py
+++ modemUnit.py
@@ -2,6 +2,7 @@ import subprocess
 import threading
 import time
 import serial
+import json
 
 
 class GPSData:
@@ -29,11 +30,16 @@ class ModemUnit:
         self.__cmd_last = ""
 
         # Serial Vals
+        self.__data_lock = False
         self.__write_lock = False
 
         # GPS Vals
         self.__gps_active = False
         self.__gps = GPSData()
+
+        # HTTP Vals
+        self.__http_code = 0
+        self.__http_data = {}
 
         self.__start_worker()
 
@@ -63,7 +69,9 @@ class ModemUnit:
                 if "OK" in newline:
                     self.__modem_power = True
                     self.__write_lock = False
-                elif "+CGNSPWR:" in newline:
+                elif "ERROR" in newline:
+                    self.__write_lock = False
+                elif "+CGNSPWR:" in newline: # GNS Pwr Notification
                     pwr = newline.split(':')[1]
                     self.__gps_active = ('1' in pwr)
                     self.__write_lock = False
@@ -82,8 +90,20 @@ class ModemUnit:
                         self.__gps.speed = float(data[6])
                     except ValueError:
                         pass
-
                     self.__write_lock = False
+                elif newline.startswith("+HTTPACTION"):  # New HTTP Data
+                    resdata = newline.split(',')
+                    code = resdata[1]
+                    self.__http_code = code
+                    self.__http_size = int(resdata[2])
+                    if code == "200":
+                        self.__exec_cmd("AT+HTTPREAD")
+                elif newline.startswith("+HTTPREAD"):
+                    http_data = self.__ser.read(self.__http_size).decode('utf-8')
+                    self.__http_data = json.loads(http_data)
+                    print("Data\n" + str(self.__http_data))
+
+
 
     def __start_worker(self):
         self.__mthread = threading.Thread(target=self.__main_thread, daemon=True)
@@ -106,6 +126,30 @@ class ModemUnit:
                     self.__modem_write(self.__cmd_queue.pop(0))
             time.sleep(0.1)
 
+    def http_get(self, url):
+        self.__exec_cmd("AT+HTTPTERM")
+        self.__exec_cmd("AT+HTTPINIT")
+        self.__exec_cmd('AT+HTTPPARA="URL","'+url+'"')
+        self.__exec_cmd('AT+HTTPPARA="CID",1')
+        self.__exec_cmd('AT+HTTPACTION=0')
+
+    def get_http_data(self):
+        return self.__http_code, self.__http_data
+
+    def data_open(self):
+        self.__exec_cmd("AT+CMEE=1")
+        self.__exec_cmd("AT+CGATT=1")
+        self.__exec_cmd("AT+CGACT=1,1")
+        self.__exec_cmd("AT+CGPADDR=1")
+        self.__exec_cmd('AT+SAPBR=3,1,"APN","super"')
+
+    def network_open(self):
+        self.__exec_cmd("AT+SAPBR=1,1")
+
+    def network_close(self):
+        self.__exec_cmd("AT+SAPBR=0,1")
+
+
 
     def start_gps(self):
         self.__exec_cmd("AT+CGNSPWR=1")  # Start GNS Modem
@@ -117,7 +161,6 @@ class ModemUnit:
 
     def get_gps(self):
         return self.__gps
-
 
     def power_toggle(self):
         subprocess.Popen(['sudo', 'raspi-gpio', 'set', '4', 'op', 'dh'])

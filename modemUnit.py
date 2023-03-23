dc37b9de227fad70d9cf53f3c6b03225324f3f72
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-03-22 19:49:06 -0600

    Modified HTTP Request Methods

--- modemUnit.py
+++ modemUnit.py
@@ -38,8 +38,11 @@ class ModemUnit:
         self.__gps = GPSData()
 
         # HTTP Vals
+        self.__http_in_progress = False
+        self.__http_request_queue = []
+        self.__http_request_last = {}
         self.__http_code = 0
-        self.__http_data = {}
+        self.__http_data = []
 
         self.__start_worker()
 
@@ -71,7 +74,7 @@ class ModemUnit:
                     self.__write_lock = False
                 elif "ERROR" in newline:
                     self.__write_lock = False
-                elif "+CGNSPWR:" in newline: # GNS Pwr Notification
+                elif "+CGNSPWR:" in newline:  # GNS Pwr Notification
                     pwr = newline.split(':')[1]
                     self.__gps_active = ('1' in pwr)
                     self.__write_lock = False
@@ -98,12 +101,25 @@ class ModemUnit:
                     self.__http_size = int(resdata[2])
                     if code == "200":
                         self.__exec_cmd("AT+HTTPREAD")
+                    else:
+                        self.__http_data.append(self.__http_code)
+
+                        if 'callback' in self.__http_request_last:
+                            self.__http_request_last['callback'](code, None)
+
+                        self.__http_in_progress = False
+                        if code == "603":  # Network Error - Attempt to Restart
+                            self.data_open()
+                            self.network_open()
+
                 elif newline.startswith("+HTTPREAD"):
-                    http_data = self.__ser.read(self.__http_size).decode('utf-8')
-                    self.__http_data = json.loads(http_data)
+                    http_data = json.loads(self.__ser.read(self.__http_size).decode('utf-8'))
+                    self.__http_data.append(http_data)
+                    if 'callback' in self.__http_request_last:
+                        self.__http_request_last['callback'](None, http_data)
+
+                    self.__http_in_progress = False
                     print("Data\n" + str(self.__http_data))
-
-
 
     def __start_worker(self):
         self.__mthread = threading.Thread(target=self.__main_thread, daemon=True)
@@ -122,19 +138,39 @@ class ModemUnit:
         while self.__worker_working:
             self.__process_input()
             if not self.__write_lock:
+                if not self.__http_in_progress:  # Perform next HTTP request
+                    if len(self.__http_request_queue) > 0:
+                        self.__perform_next_http()
+
                 if len(self.__cmd_queue) > 0:
                     self.__modem_write(self.__cmd_queue.pop(0))
             time.sleep(0.1)
 
-    def http_get(self, url):
-        self.__exec_cmd("AT+HTTPTERM")
-        self.__exec_cmd("AT+HTTPINIT")
-        self.__exec_cmd('AT+HTTPPARA="URL","'+url+'"')
-        self.__exec_cmd('AT+HTTPPARA="CID",1')
-        self.__exec_cmd('AT+HTTPACTION=0')
+    # HTTP Functions
+    def __perform_next_http(self):
+        if not self.__http_in_progress:
+            self.__http_in_progress = True
+            next_request = self.__http_request_queue.pop(0)
+            self.__http_request_last = next_request
+            self.__exec_cmd("AT+HTTPTERM")
+            self.__exec_cmd("AT+HTTPINIT")
+            self.__exec_cmd('AT+HTTPPARA="URL","' + next_request['url'] + '"')
+            self.__exec_cmd('AT+HTTPPARA="CID",1')
+            self.__exec_cmd('AT+HTTPACTION=' + str(next_request['action']))
+            return True
+        return False
 
-    def get_http_data(self):
-        return self.__http_code, self.__http_data
+    def __http_request(self, url, action):
+        self.__http_request_queue.append({'url': url, 'action': action})
+
+    def http_get(self, url):
+        self.__http_request(url, 0)
+
+    def get_http_last(self):
+        return self.__http_code, self.__http_data[0]
+
+    def get_http_response(self, i):
+        return self.__http_data[i]
 
     def data_open(self):
         self.__exec_cmd("AT+CMEE=1")
@@ -150,7 +186,7 @@ class ModemUnit:
         self.__exec_cmd("AT+SAPBR=0,1")
 
 
-
+    # GPS and Location Functions
     def start_gps(self):
         self.__exec_cmd("AT+CGNSPWR=1")  # Start GNS Modem
         self.__exec_cmd("AT+CGNSURC=2")  # Start GNS Reporting

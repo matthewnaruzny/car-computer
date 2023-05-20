ba07866dcfb089e004c45f764927f56dcb79fe7f
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-05-19 21:00:05 -0600

    Add SOS ping

---
+++ networkCommunication.py
@@ -0,0 +1,11 @@
+from modemUnit import ModemUnit
+
+
+class NetworkCommunication:
+    def __init__(self, imei, mUnit):
+        assert isinstance(mUnit, ModemUnit)
+        self.imei = imei
+        self.mUnit = mUnit
+
+    def sendSOS(self):
+        self.mUnit.http_get("http://t.upnorthdevelopers.com:5055/?id=" + str(self.imei) + "&alarm=sos")

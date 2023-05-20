ed0d7f344c62977b74df8f0c8a4e7dd5f36cc780
Matthew Naruzny <matthewnaruzny@gmail.com>
2023-05-19 22:24:26 -0600

    Set Pin Numbering Mode

--- safetyCheck.py
+++ safetyCheck.py
@@ -9,6 +9,7 @@ class SafetyCheck:
     def __init__(self, networker):
         assert isinstance(networker, NetworkCommunication)
         self.networker = networker
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def __start_thread(self):
